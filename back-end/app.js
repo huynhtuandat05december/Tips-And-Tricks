d1e9944cda021bbf39e1f1f69515793e6a55a866
huynhtuandat05december <huynhtuandat05december@gmail.com>
2022-06-23 09:13:48 +0700

    Add usefull package

--- back-end/app.js
+++ back-end/app.js
@@ -1,15 +1,21 @@
 const express = require("express");
+const helmet = require("helmet");
+const morgan = require("morgan");
 const createError = require("http-errors");
 const userRouter = require("./router/user");
+
 const app = express();
+
+app.use(helmet()); // protected header
+app.use(morgan("common")); // write log in console when request
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", function (req, res, next) {
   res.json("hello");
 });
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use("/user", userRouter);
+app.use("/api/v1/user", userRouter);
 
 app.use((req, res, next) => {
   console.log("middleware");
